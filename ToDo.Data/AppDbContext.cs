cd02b4a52a700626005b5d95531b5cb7b9396ba1
travis@travestyofcode.com <travis@travestyofcode.com>
2025-02-09 19:08:57 -0500

    Add project files.

---
+++ ToDo.Data/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace ToDo.Data;
+
+internal class AppDbContext : IdentityDbContext<AppUser>
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+}
+
+internal class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        IConfigurationBuilder configBuilder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", true)
+            .AddEnvironmentVariables()
+            .AddCommandLine(args);
+
+        var config = configBuilder.Build();
+
+        var oBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+        oBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+
+        return new AppDbContext(oBuilder.Options);
+    }
+}
