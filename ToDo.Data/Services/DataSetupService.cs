71f7025a097ccb8dad2e1b3bb51e6b18368353ed
travis@travestyofcode.com <travis@travestyofcode.com>
2025-02-16 08:36:35 -0500

    Adds static method to register db context and MediatR.

---
+++ ToDo.Data/Services/DataSetupService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ToDo.Data.Services;
+
+public static class DataSetupService
+{
+    public static IServiceCollection AddDataServices(IServiceCollection services, string connectionString)
+    {
+        services.AddDbContext<AppDbContext>(o => o.UseSqlServer(connectionString));
+
+        services.AddDefaultIdentity<AppUser>(o => o.SignIn.RequireConfirmedAccount = true)
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<AppDbContext>() // Not sure if this is needed or set by AddDefaultIdentity.
+            .AddDefaultTokenProviders(); // Not sure if this is needed or set by AddDefaultIdentity.
+
+        services.AddMediatR(o => o.RegisterServicesFromAssembly(System.Reflection.Assembly.GetExecutingAssembly()));
+
+        return services;
+    }
+}
