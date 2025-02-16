0cfe6809e0e083e0699367b756d7d7c2b9976f7a
travis@travestyofcode.com <travis@travestyofcode.com>
2025-02-16 11:34:01 -0500

    Adds simple Results class.

---
+++ ToDo.Data/Services/Result.cs
@@ -0,0 +1,53 @@
+﻿namespace ToDo.Data.Services;
+
+public class Result
+{
+    public int StatusCode { get; set; }
+    public bool WasSuccessful => StatusCode >= 200 && StatusCode <= 299;
+
+    public Result()
+    {
+    }
+
+    public Result(int statusCode) : this()
+    {
+        StatusCode = statusCode;
+    }
+
+    public static BadRequestResult BadRequest() => new BadRequestResult();
+    public static NotFoundResult NotFound() => new NotFoundResult();
+    public static ForbiddenResult Forbidden() => new ForbiddenResult();
+    public static ServerErrorResult ServerError() => new ServerErrorResult();
+}
+
+public class Result<T> : Result
+{
+    public T? Value { get; set; }
+
+    public Result() : base()
+    {
+
+    }
+
+    public static implicit operator Result<T>(BadRequestResult result) => new Result<T> { StatusCode = 400 };
+    public static implicit operator Result<T>(NotFoundResult result) => new Result<T> { StatusCode = 404 };
+    public static implicit operator Result<T>(ForbiddenResult result) => new Result<T> { StatusCode = 403 };
+    public static implicit operator Result<T>(ServerErrorResult result) => new Result<T> { StatusCode = 500 };
+}
+
+public class BadRequestResult : Result
+{
+    public BadRequestResult() : base(400) { }
+}
+public class NotFoundResult : Result
+{
+    public NotFoundResult() : base(404) { }
+}
+public class ForbiddenResult : Result
+{
+    public ForbiddenResult() : base(403) { }
+}
+public class ServerErrorResult : Result
+{
+    public ServerErrorResult() : base(500) { }
+}
